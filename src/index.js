2f5385087526ca803a995c7ea08d95fda43951c2
Dylan Palmboom <nofxboy1234@gmail.com>
2024-06-06 12:10:33 +0200

    Clean up unused files and imports

--- src/index.js
+++ src/index.js
@@ -1,32 +1,3 @@
-import myName from './myName';
-import { functionOne, functionTwo } from './myModule';
-import './style.css';
-import Icon from './icon.png';
+// orderTotal is given an order and it outputs a total amount of all the
+// items that are contained in that order
 
-import printMe from './print.js';
-
-function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
-
-  // Lodash, now imported by this script
-  element.innerHTML = 'Hello webpack';
-  element.classList.add('hello');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
-  element.appendChild(btn);
-
-  console.log(myName('Dylan'));
-  console.log(functionOne());
-  console.log(functionTwo());
-
-  return element;
-}
-
-document.body.appendChild(component());
