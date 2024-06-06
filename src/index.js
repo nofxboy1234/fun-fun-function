d37fc6944d444c9cc849306ded444d062c29f549
Dylan Palmboom <nofxboy1234@gmail.com>
2024-06-06 13:09:21 +0200

    Add quantity

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@
 // items that are contained in that order
 const someOrder = {
   items: [
-    { name: 'Dragon food', price: 8 },
-    { name: 'Dragon cage (small)', price: 800 },
+    { name: 'Dragon food', price: 8, quantity: 8 },
+    { name: 'Dragon cage (small)', price: 800, quantity: 2 },
   ],
 };
 
 const orderTotal = (order) =>
-  order.items.reduce((prev, cur) => prev + cur.price, 0);
+  order.items.reduce((prev, cur) => prev + (cur.price * cur.quantity), 0);
 
 const result = orderTotal(someOrder);
 
