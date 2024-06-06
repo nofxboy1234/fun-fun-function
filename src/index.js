013c31496baf9aec163ab2ff51091167296bf3d3
Dylan Palmboom <nofxboy1234@gmail.com>
2024-06-06 13:03:09 +0200

    Add starting code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 // orderTotal is given an order and it outputs a total amount of all the
 // items that are contained in that order
+const someOrder = {
+  items: [
+    { name: 'Dragon food', price: 8 },
+    { name: 'Dragon cage (small)', price: 800 },
+  ],
+};
 
+const orderTotal = (order) =>
+  order.items.reduce((prev, cur) => prev + cur.price, 0);
+
+const result = orderTotal(someOrder);
+
+console.log('end');
