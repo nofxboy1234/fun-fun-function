a8c58500effac14bbf3fe03fcf652d7a238fad27
Dylan Palmboom <nofxboy1234@gmail.com>
2024-06-10 13:02:02 +0200

    Refactor mocksNotEvil

--- mocksNotEvil.js
+++ mocksNotEvil.js
@@ -1,11 +1,11 @@
-// const express = require('express');
-// const app = express();
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
-// });
-// app.listen(3000, function () {
-//   console.log('Example app listening on port 3000!');
-// });
+const express = require('express');
+const app = express();
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
 
 const hello = (req, res) => res.send('Hello World!');
 {
@@ -23,23 +23,13 @@ const hello = (req, res) => res.send('Hello World!');
 }
 
 const handleListen = (log, port) => {
-  const a = 1;
   log(port);
-  // log(`Example app listening on port ${port}!`);
 };
 {
-  const expected = 'Example app listening on port 3000!';
+  const expected = 3000;
   const msg = `should call log() with ${expected}`;
-  // const res = {
-  //   send: (actual) => {
-  //     if (actual !== expected) {
-  //       throw new Error(`NOT OK ${msg}`);
-  //     }
-  //     console.log(`OK: ${msg}`);
-  //   },
-  // };
   const log = (actual) => {
-    if (`Example app listening on port ${actual}!` !== expected) {
+    if (actual !== expected) {
       throw new Error(`NOT OK ${msg}`);
     }
     console.log(`OK: ${msg}`);
