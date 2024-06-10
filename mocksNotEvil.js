b562b3c38fb4a81f46d8f68ca1a845f7d55101c1
Dylan Palmboom <nofxboy1234@gmail.com>
2024-06-10 11:38:36 +0200

    Update mocksNotEvil

--- mocksNotEvil.js
+++ mocksNotEvil.js
@@ -6,3 +6,18 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+const hello = (req, res) => res.send('Hello World!');
+{
+  const expected = 'Hello World!';
+  const msg = `should call .send() with ${expected}`;
+  const res = {
+    send: (actual) => {
+      if (actual !== expected) {
+        throw new Error(`NOT OK ${msg}`);
+      }
+      console.log(`OK: ${msg}`);
+    },
+  };
+  hello({}, res);
+}
