6ccf2ac32406ee1e7ebf539ce4189a98dfcf121b
Dylan Palmboom <nofxboy1234@gmail.com>
2024-06-10 10:19:07 +0200

    Add objectFutureComputations

---
+++ objectFutureComputations.js
@@ -0,0 +1,43 @@
+// sugar for console.log we'll use later
+const log = (msg) => console.log(msg);
+const call = (fn, ...args) => ({ fn, args });
+const put = (msg) => ({ msg });
+// imported from I/O API
+const sendMessage = (msg) => Promise.resolve('some response');
+// imported from state handler/Reducer
+const handleResponse = (response) => ({
+  type: 'RECEIVED_RESPONSE',
+  payload: response,
+});
+const handleError = (err) => ({
+  type: 'IO_ERROR',
+  payload: err,
+});
+function* sendMessageSaga(msg) {
+  try {
+    const response = yield call(sendMessage, msg);
+    yield put(handleResponse(response));
+  } catch (err) {
+    yield put(handleError(err));
+  }
+}
+
+const iter = sendMessageSaga('Hello, world!');
+// Returns an object representing the status and value:
+const step1 = iter.next();
+log(step1);
+/* =>
+{
+  done: false,
+  value: {
+    fn: sendMessage
+    args: ["Hello, world!"]
+  }
+}
+*/
+
+const {
+  value: { fn, args },
+} = step1;
+log(fn);
+log(args);
