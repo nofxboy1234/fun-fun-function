c75d91a3c1ebccda371906c95d55023c14280eba
Dylan Palmboom <nofxboy1234@gmail.com>
2024-06-10 12:54:30 +0200

    Add mock for handleListen()

--- mocksNotEvil.js
+++ mocksNotEvil.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+// const express = require('express');
+// const app = express();
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!');
+// });
 
 const hello = (req, res) => res.send('Hello World!');
 {
@@ -20,4 +20,29 @@ const hello = (req, res) => res.send('Hello World!');
     },
   };
   hello({}, res);
+}
+
+const handleListen = (log, port) => {
+  const a = 1;
+  log(port);
+  // log(`Example app listening on port ${port}!`);
+};
+{
+  const expected = 'Example app listening on port 3000!';
+  const msg = `should call log() with ${expected}`;
+  // const res = {
+  //   send: (actual) => {
+  //     if (actual !== expected) {
+  //       throw new Error(`NOT OK ${msg}`);
+  //     }
+  //     console.log(`OK: ${msg}`);
+  //   },
+  // };
+  const log = (actual) => {
+    if (`Example app listening on port ${actual}!` !== expected) {
+      throw new Error(`NOT OK ${msg}`);
+    }
+    console.log(`OK: ${msg}`);
+  };
+  handleListen(log, 3000);
 }
